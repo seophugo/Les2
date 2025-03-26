e0bd21c2e454e6bc2459aca880f4618b858fc7de
seophugo <hugo.lanting@gmail.com>
2025-03-26 12:36:48 +0100

    project setup

---
+++ Assets/CollisionDetection.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("het werkt");
+        }
+    }
+}
